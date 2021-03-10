f1c2fcf3358b6b9e8f3a99162fdc621514562de0
Abby Brown <abby_brown3@baylor.edu>
2021-03-10 15:46:18 -0600

    init

---
+++ panda_series.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+grades = pd.Series([87, 100, 94])
+
+print(grades)
+
+grades = pd.Series(98.6, range(3))
+
+a = grades[0]
+grades.count()
+grades.mean()
+grades.min(0)
+grades.max()
+grades.std()
+
+
+grades.describe()
+
+grades = pd.Series([87, 100, 94], index=["Wally", "Eva", "Sam"])
+print(grades)
+
+
+grades.Wally
+
+grades.dtype
+
+grades.values
